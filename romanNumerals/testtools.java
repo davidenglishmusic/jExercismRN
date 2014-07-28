10cdf72c3ae00035f21fed827820a8470e47c07a
David English <david@operaonfilm.com>
2014-07-28 13:25:12 -0700

    Switched some class variables to private

--- romanNumerals/testtools.java
+++ romanNumerals/testtools.java
@@ -2,9 +2,9 @@ package romanNumerals;
 
 class TestTools
 {
-  public static final String ANSI_RESET = "\u001B[0m";
-  public static final String ANSI_GREEN = "\u001B[32m";
-  public static final String ANSI_RED = "\u001B[31m";
+  private static final String ANSI_RESET = "\u001B[0m";
+  private static final String ANSI_GREEN = "\u001B[32m";
+  private static final String ANSI_RED = "\u001B[31m";
 
   public TestTools()
   {
